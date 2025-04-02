ce6c585b10e6e300d2bdb8c0c83f39ee7886fa45
NooBIE-Nilay <nbanerjee02.asn@gmail.com>
2025-04-02 15:47:11 +0530

    Added Rust Function - isEven(); -- checks if a no is even or not.

---
+++ rust-1-1/src/main.rs
@@ -0,0 +1,13 @@
+
+
+fn main() {
+ let name: &str = "NooBIE";
+ let _str = String::from("NooBIE");
+ println!("{}",name);
+}
+fn _is_even(number: i32)-> bool {
+    if number % 2 == 0{
+        return  true;
+    }
+    return false;
+}
